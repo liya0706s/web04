4cce30b77816030b431e888ae60a8970cac5f5dd
Angie <liya0706s@gmail.com>
2024-03-04 23:07:40 +0800

    0304打db囉

---
+++ api/db.php
@@ -0,0 +1,16 @@
+<?php
+date_default_timezone_set("Asia/Taipei");
+session_start();
+
+class DB{
+    protected $pdo="mysql:host=localhost;charset=utf8;dbname=db04";
+    protected $dsn;
+    protected $table;
+
+    
+}
+
+
+
+
+?>
