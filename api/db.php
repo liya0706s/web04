f863cc999fbdee15debdb0745bb27e427f8e39b2
Angie <liya0706s@gmail.com>
2024-03-05 08:42:57 +0800

    fix: db bug

--- api/db.php
+++ api/db.php
@@ -2,131 +2,144 @@
 date_default_timezone_set("Asia/Taipei");
 session_start();
 
-class DB{
-    protected $pdo="mysql:host=localhost;charset=utf8;dbname=db04";
-    protected $dsn;
+class DB
+{
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db04";
+    protected $pdo;
     protected $table;
 
     public function __construct($table)
     {
-        $this->table=$table;
-        $this->pdo=new PDO($this->dsn,'root','');
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
     }
 
-    function q($sql){
-        $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-    }
-    private function a2s($array){
-        foreach($array as $col => $value){
-            $sql="`$col`='$value'";
-        }
+    function q($sql)
+    {
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
-    private function sql_all($sql, $array, $other){
-        if(isset($this->table) && !empty($this->table)){
-            if(!empty($array)){
-                $tmp=$this->a2s($sql, $array, $other);
-                $sql=" where ".join(" && ", $tmp);
-            }else{
-                $sql.= " $array";
+    private function a2s($array)
+    {
+        foreach ($array as $col => $value) {
+            $tmp[] = "`$col`='$value'";
+        }
+        return $tmp;
+    }
+    private function sql_all($sql, $array, $other)
+    {
+        if (isset($this->table) && !empty($this->table)) {
+            if (!empty($array)) {
+                $tmp = $this->a2s($array);
+                $sql .= " where " . join(" && ", $tmp);
+            } else {
+                $sql .= " $array";
             }
-            $sql.= $other;
-            return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+            $sql .= $other;
+            return $sql;
         }
     }
 
-    function all($where='', $other=''){
-        $sql="select * form `$this->table` ";
-        $sql=$this->sql_all($sql, $where, $other);
+    function all($where = '', $other = '')
+    {
+        $sql = "select * from `$this->table` ";
+        $sql = $this->sql_all($sql, $where, $other);
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
-    function count($where='', $other=''){
-        $sql="select count(*) from `$this->table` ";
-        $sql=$this->sql_all($sql, $where, $other);
+    function count($where = '', $other = '')
+    {
+        $sql = "select count(*) from `$this->table` ";
+        $sql = $this->sql_all($sql, $where, $other);
         return $this->pdo->query($sql)->fetchColumn();
     }
-    private function math($math, $col, $where, $other){
-        $sql="select $math(`$col`) from `$this->table` ";
-        $sql=$this->sql_all($sql, $where, $other);
+    private function math($math, $col, $where='', $other='')
+    {
+        $sql = "select $math(`$col`) from `$this->table` ";
+        $sql = $this->sql_all($sql, $where, $other);
         return $this->pdo->query($sql)->fetchColumn();
     }
-    function sum($col='', $where='', $other=''){
+    function sum($col = '', $where = '', $other = '')
+    {
         return $this->math('sum', $col, $where, $other);
     }
-    function max($col, $where='', $other=''){
+    function max($col, $where = '', $other = '')
+    {
         return $this->math('max', $col, $where, $other);
     }
-    function min($col, $where='', $other=''){
+    function min($col, $where = '', $other = '')
+    {
         return $this->math('min', $col, $where, $other);
     }
 
-    function find($id){
-        $sql="select * from `$this->table` ";
-        if(is_array($id)){
-            $tmp=$this->a2s($id);
-            $sql.= " where ".join(" && ",$tmp);
-        }else if(is_numeric($id)){
-            $sql.= " where `id`='$id'";
+    function find($id)
+    {
+        $sql = "select * from `$this->table` ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= " where " . join(" && ", $tmp);
+        } else if (is_numeric($id)) {
+            $sql .= " where `id`='$id'";
         }
-        $row=$this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+        $row = $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
         return $row;
     }
 
-    function del($id){
-        $sql="delete from `$this->table` where ";
-        if(is_array($id)){
-            $tmp=$this->a2s($id);
-            $sql.= join(" && ", $tmp);
-        }else if(is_numeric($id)){
-            $sql.= " `id`='$id'";
+    function del($id)
+    {
+        $sql = "delete from `$this->table` where ";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= join(" && ", $tmp);
+        } else if (is_numeric($id)) {
+            $sql .= " `id`='$id'";
         }
         return $this->pdo->exec($sql);
     }
 
-    function save($array){
-        if(isset($array['id'])){
-            $sql="update from `$this->table` set ";
-            if(!empty($array)){
-                $tmp=$this->a2s($array);
+    function save($array)
+    {
+        if (isset($array['id'])) {
+            $sql = "update `$this->table` set ";
+            if (!empty($array)) {
+                $tmp = $this->a2s($array);
             }
-            $sql.=join(",", $tmp);
-            $sql.= " where `id`='{$array['id']}'";
-        }else{
-            $sql="insert into `$this->table` ";
-            $cols="(`" .join("`,`", array_keys($array)) . "`)";
-            $vals="('".join("','", $array). "')";
-            $sql = $sql. $cols." values ". $vals;
+            $sql .= join(",", $tmp);
+            $sql .= " where `id`='{$array['id']}'";
+        } else {
+            $sql = "insert into `$this->table` ";
+            $cols = "(`" . join("`,`", array_keys($array)) . "`)";
+            $vals = "('" . join("','", $array) . "')";
+            $sql = $sql . $cols . " values " . $vals;
         }
-        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+        return $this->pdo->exec($sql);
     }
 }
 // DB到這裡結束
 
-function dd($array){
-    echo "<pr>";
+function dd($array)
+{
+    echo "<pre>";
     print_r($array);
-    echo "</pr>";
+    echo "</pre>";
 }
 
-function to($url){
+function to($url)
+{
     header("location:$url");
 }
 
-$Log=new DB('log');
-$News=new DB('news');
-$Que=new DB('que');
-$Total=new DB('total');
-$User=new DB('user');
+$Log = new DB('log');
+$News = new DB('news');
+$Que = new DB('que');
+$Total = new DB('total');
+$User = new DB('user');
 
-if(!isset($_SESSION['visited'])){
-    if($Total->count(['date'=>date("Y-m-d")])>0){
-        $total=$Total->find(['date'=>date("Y-m-d")]);
+if (!isset($_SESSION['visited'])) {
+    if ($Total->count(['date' => date("Y-m-d")]) > 0) {
+        $total = $Total->find(['date' => date("Y-m-d")]);
         $total['total']++;
         $Total->save($total);
-    }else{
-        $Total->save(['total'=>1, 'date'=>date("Y-m-d")]);
+    } else {
+        $Total->save(['total' => 1, 'date' => date("Y-m-d")]);
     }
-    $_SESSION['visited']=1;
+    $_SESSION['visited'] = 1;
 }
-
-?>
