adde79f2564542bd0d6ddb77c9b0156c3cdd86ed
Angie <liya0706s@gmail.com>
2024-03-09 08:28:06 +0800

    分類網誌js-抓標題超連結的api還沒默完

--- front/po.php
+++ front/po.php
@@ -1,7 +1,22 @@
+<style>
+    .type-item{
+        display: block;
+    }
+
+    .types, .news-list{
+        display:inline-block;
+        vertical-align: top;
+    }
+
+    .list-items{
+        width: 600px;
+    }
+</style>
+
 <div>
     目前位置 : 首頁 > 分類網誌 > <span class="type">健康新知</span>
 </div>
-<fieldset>
+<fieldset class="types">
     <legend>分類網誌</legend>
     <a class="type-item" data-type="1">健康新知</a>
     <a class="type-item" data-type="2">菸害防治</a>
@@ -9,21 +24,29 @@
     <a class="type-item" data-type="4">慢性病防治</a>
 </fieldset>
 
-<fieldset>
+<fieldset class="news-list">
     <legend>文章列表</legend>
     <div class="list-items" style="display:none"></div>
     <div class="article"></div>
 </fieldset>
 
 <script>
-    // getList(1)
-    
-    $(".type-item").on('click', function(){
-        $(".type").text($(this).text())
-        let type=$(this).data('type')
+    getList(1)
+$(".type-item").on('click', function(){
+    $(".type").text($(this).text())
 
-        // getList(type)
+    let type=$(this).data('type');
+    getList(type)
+})
+
+function getList(type){
+    $.get('./api/get_list.php',{type},(list)=>{
+        $(".list-items").html(list);
+        $("article").hide();
+        $(".list-items").show();
     })
+}
+
 
 
 </script>
