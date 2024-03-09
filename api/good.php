ebf9d7d2c84fcb01baab7e574da7be149d2f1ef7
Angie <liya0706s@gmail.com>
2024-03-09 21:01:33 +0800

    feat:最新文章按讚js和api都完成!

--- api/good.php
+++ api/good.php
@@ -1,16 +1,17 @@
 <?php
 include_once "db.php";
 
+// 找到要按讚的那一筆
 $news=$News->find($_POST['news']);
 
+// 執行good()函式，有POST傳值的news和使用者紀錄的帳號，按讚紀錄表執行刪除
 if($Log->count(['news'=>$_POST['news'], 'acc'=>$_SESSION['user']])>0){
     $Log->del(['news'=>$_POST['news'], 'acc'=>$_SESSION['user']]);
+    // 新聞文章資料表新聞欄減一
     $news['good']--;
 }else{
     $Log->save(['news'=>$_POST['news'], 'acc'=>$_SESSION['user']]);
     $news['good']++;
 }
-
 $News->save($news);
-
 ?>
--- front/news.php
+++ front/news.php
@@ -35,11 +35,10 @@
                 <!-- 根據登入狀態顯示按讚的程式 -->
                 <td class="ct">
                     <?php
-                    if (isset($_SESSION['user'])) {
-                        if ($Log->count(['news' => $row['id'], 'acc' => $_SESSION['user']]) > 0) {
-                            // 這篇文章$row['id'] 如果count()==1有代表按讚過
+                    if(isset($_SESSION['user'])){
+                        if($Log->count(['news'=>$row['id'], 'acc'=>$_SESSION['user']])){
                             echo "<a href='Javascript:good({$row['id']})'> 收回讚 </a>";
-                        } else {
+                        }else{
                             echo "<a href='Javascript:good({$row['id']})'> 讚 </a>";
                         }
                     }
@@ -71,15 +70,13 @@
 
 <script>
     $(".title").on('click', function() {
-        let id = $(this).data('id')
-        $(`#s${id},#a${id}`).toggle();
-        // 用``允許同時寫字串和變數和一般的引號不一樣
+        let id = $(this).data('id');
+        $(`#s${id}, #a${id}`).toggle();
     })
 
-    // 這裡的參數news是，文章id--$row['id']
     function good(news){
-        $.post('./api/good.php',{news},()=>{
-        location.reload();
+        $.post('./api/good.php',{news}, ()=>{
+            location.reload();
         })
     }
 </script>
