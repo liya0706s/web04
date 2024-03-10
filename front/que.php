a75a45cad4e393780fa33815dbfbd91a09dce9de
Angie <liya0706s@gmail.com>
2024-03-10 22:35:39 +0800

    feat:step18-前台我要投票./front/vote

--- front/que.php
+++ front/que.php
@@ -1,6 +1,6 @@
 <fieldset>
     <legend>目前位置 : 首頁 > 問卷調查 </legend>
-    <table class="ct" style="width:90%">
+    <table class="ct" style="width:90%; margin: auto;">
         <tr class="clo">
             <th>編號</th>
             <th>問卷題目</th>
@@ -9,21 +9,22 @@
             <th>狀態</th>
         </tr>
         <?php
-        $rows = $Que->all(['subject_id' => 0]);
-        foreach ($rows as $key => $val) {
+        $ques = $Que->all(['subject_id'=>0]);
+        foreach ($ques as $key => $que) {
         ?>
             <tr>
-                <td><?= $key + 1; ?></td>
-                <td><?= $val['text']; ?></td>
-                <td><?= $val['vote']; ?></td>
+                <td><?= $key+1; ?></td>
+                <td><?= $que['text']; ?></td>
+                <td><?= $que['vote']; ?></td>
                 <td>
-                    <a href="?do=result&id=<?= $val['id']; ?>">結果</a>
+                    <a href="?do=result&id=<?= $que['id']; ?>">結果</a>
                 </td>
                 <td>
+                    
                     <?php
                     if (isset($_SESSION['user'])) {
-                        echo "<a href='?do=vote&id={$val['id']}'> 參與投票 </a>";
-                    } else {
+                        echo "<a href='?do=vote&id={$que['id']}'>參與投票</a>";
+                    }else{
                         echo "請先登入";
                     }
                     ?>
