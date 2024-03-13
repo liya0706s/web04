fcee22aad4f42049008fa28e859fc33f93c62b0d
Angie <liya0706s@gmail.com>
2024-03-13 14:48:35 +0800

    fix:第三欄先判斷有無登入紀錄，再判斷有數到一筆案讚紀錄的news和acc, 可以按讚和收回讚

--- front/news.php
+++ front/news.php
@@ -32,7 +32,7 @@
                 <td class="ct">
                     <?php
                     if (isset($_SESSION['user'])) {
-                        if ($Log->count(['news' => $row['id'], 'acc' => $_SESSION['user']])) {
+                        if ($Log->count(['news' => $row['id'], 'acc' => $_SESSION['user']])>0) {
                             echo "<a href='Javascript:good({$row['id']})'> 收回讚 </a>";
                         } else {
                             echo "<a href='Javascript:good({$row['id']})'> 讚 </a>";
